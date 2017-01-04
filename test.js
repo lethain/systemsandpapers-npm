7191f2654868d7ff148647b1fbee1b0a9ba3a78b
Will Larson <lethain@gmail.com>
2017-01-03 19:40:17 -0800

    And stub tests, I am sorry.

---
+++ test.js
@@ -0,0 +1,8 @@
+var sap = require('./index')
+var sid = "1"
+var token = "fake-token"
+function solve(input) {
+    // change input into output somehow
+    return input
+}
+sap.attempt(sid, token, solve)
